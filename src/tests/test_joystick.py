ee84e55804154a36392134dd69b097fab1555ccb
Artyom Oganesyan <artyogan002@gmail.com>
2025-05-12 18:13:19 +0300

    added joystick deadzone processing and modified control function

--- src/tests/test_joystick.py
+++ src/tests/test_joystick.py
@@ -1,19 +1,32 @@
 import mujoco as mj
 from mujoco.glfw import glfw
+import numpy as np
+import time
 
 def get_jaxes():
     if glfw.joystick_present(glfw.JOYSTICK_1):
         axes = glfw.get_joystick_axes(glfw.JOYSTICK_1)
         buttons = glfw.get_joystick_buttons(glfw.JOYSTICK_1)
-    return axes
+    return axes, buttons
 
-def update_control_from_joystick(model, data):
+def apply_deadzone(val, threshold=0.1):
+    return 0 if abs(val) < threshold else val
+
+def update_control_from_joystick(robot, window):
     if glfw.joystick_present(glfw.JOYSTICK_1):
         axes, _ = glfw.get_joystick_axes(glfw.JOYSTICK_1)
         buttons, _ = glfw.get_joystick_buttons(glfw.JOYSTICK_1)
         
-        val = -axes[0]
-        data.ctrl[0] = val
+        val = -(axes[0]*1.5) ** 3
+        control = np.array([val, 0., 0., 0., 0., 0.])
+        robot.set_ctrl(control)
+
+        if (glfw.PRESS == buttons[0]):
+            robot.change_gripper_state()
+
+        if (glfw.PRESS == buttons[1]):
+            glfw.set_window_should_close(window, 1)
+
 
 class JoystickHandler:
     def __init__(self):
